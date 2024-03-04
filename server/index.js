774e95db8ea92d5de3e2dace1dc3f936243a4ac5
lemusm3 <lemusm3@newplatz.edu>
2024-03-04 12:40:47 -0500

    updates the dependencies

--- server/index.js
+++ server/index.js
@@ -1,1 +1,14 @@
-console.log("Hello World")
+const express = require('express');
+/* B"H
+*/
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello New Paltz!')
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT}`)
+});
