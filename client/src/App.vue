f689345955372e597db1b94745e22f13ed0dbc34
lemusm3 <lemusm3@newplatz.edu>
2024-03-17 18:02:25 -0400

    Add App.vue component with NavBar and RouterView

---
+++ client/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import NavBar from './components/NavBar.vue';
+
+</script>
+
+<template>
+  <div class="wrapper">
+    <NavBar />
+    <div class="container">
+      <RouterView /> 
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .wrapper {
+    margin-right: 2rem;
+  }
+</style>
