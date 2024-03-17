515a644bdd5f0ef6a141542dd06ef3cf984bc016
lemusm3 <lemusm3@newplatz.edu>
2024-03-17 18:04:29 -0400

    Add server/index.js file with basic Express setup

--- server/index.js
+++ server/index.js
@@ -1,1 +1,18 @@
+<<<<<<< HEAD
 console.log("Hello World")
+=======
+const express = require('express');
+/* B"H
+*/
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello New Paltz!')
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT}`)
+});
+>>>>>>> efbb3a7 (Add server/index.js file with basic Express setup)
