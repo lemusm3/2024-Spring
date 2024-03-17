d950d35fbea539c49a195e50e3008c968cc795bb
lemusm3 <lemusm3@newplatz.edu>
2024-03-17 18:03:54 -0400

    Add product list page with dynamic data and shopping cart functionality

---
+++ client/src/pages/products/index.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+    import { ref, computed } from 'vue'
+    import { type Product, getProducts } from "@/model/products";
+    import ProductCard from '@/components/ProductCard.vue';
+    import FlyOut from '@/components/FlyOut.vue';
+    import { addToCart, isOpen } from '@/viewModel/cart';
+    import ShoppingCart from '@/components/ShoppingCart.vue';
+
+
+    const products = ref([] as Product[])
+    
+    products.value = getProducts()
+
+
+</script>
+
+<template>
+    <div class="product-list">
+        <ProductCard    v-for="product in products" :key="product.id"
+                        :product="product" @addToCart="addToCart"
+                    />
+    </div>
+
+    <FlyOut :isOpen="isOpen" >
+        <ShoppingCart />
+    </FlyOut>
+</template>
+
+<style scoped>
+    .product-list {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+    }
+</style>
