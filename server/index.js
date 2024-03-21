665da5b69757e52f0f14862a814ac4e01d0e2b71
lemusm3 <lemusm3@newplatz.edu>
2024-03-21 14:01:09 -0400

    Starting Express

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@
 console.log("Hello World")
 =======
 const express = require('express');
+const users = require('./controllers/users');
 /* B"H
 */
 
@@ -10,9 +11,15 @@ const PORT = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello New Paltz!')
-});
+})
+.use('/users', users)
+
 
 app.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT}`)
+<<<<<<< HEAD
 });
 >>>>>>> efbb3a7 (Add server/index.js file with basic Express setup)
+=======
+});
+>>>>>>> 96fc89f (Starting Express)
