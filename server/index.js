1224bb23431df6ba82226b2042a4f01e5ead7bc7
lemusm3 <lemusm3@newplatz.edu>
2024-04-04 13:55:22 -0400

    Refactor user controller and model

--- server/index.js
+++ server/index.js
@@ -9,17 +9,27 @@ const users = require('./controllers/users');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello New Paltz!')
-})
-.use('/api/vl/users', users)
+app
+  .use(express.json())
+
+app
+  .get('/', (req, res) => {
+    res.send('Hello New Paltz!')
+  })
+  .use('/api/v1/users', users)
+
+// Error handling
 
 
 app.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT}`)
+<<<<<<< HEAD
 <<<<<<< HEAD
 });
 >>>>>>> efbb3a7 (Add server/index.js file with basic Express setup)
 =======
 });
 >>>>>>> 96fc89f (Starting Express)
+=======
+});
+>>>>>>> dfc1452 (Refactor user controller and model)
