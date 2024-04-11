cb8023e7d4734ceb98297de368c74524cab1e2ce
lemusm3 <lemusm3@newplatz.edu>
2024-04-11 13:57:48 -0400

    Promisifying file access

--- server/models/users.js
+++ server/models/users.js
@@ -1,8 +1,60 @@
 /* B"H
 */
+const fs = require('fs');
+
+const fileName = __dirname + '/../data/users.json';
 
 /** @type { { items: User[] } } */
-const data = require('../data/users.json');
+let data //= require('../data/users.json');
+
+function isFileAccessible(fileName) {
+    return new Promise((resolve, reject) => {
+        fs.access(fileName, fs.constants.F_OK, (err) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve();
+        });
+    });
+}
+
+function readFile(fileName) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(fileName, 'utf8', (err, content) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(content);
+        });
+    });
+}
+
+function writeFile(fileName, content) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(fileName, content, (err) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve();
+        });
+    });
+}
+
+isFileAccessible(fileName)
+    .then(() => readFile(fileName))
+    .then(content => {
+        data = JSON.parse(content);
+    })
+    .catch(err => {
+        console.error(err);
+    });
+
+function save() {
+    return writeFile(fileName, JSON.stringify( data , null, 2) );
+}
 
 /**
  * @typedef {import('../../client/src/model/users').User} User
@@ -43,6 +95,7 @@ function search(q) {
 function add(user) {
     user.id = data.items.length + 1;
     data.items.push(user);
+    save().catch(console.error);
     return user;
 }
 
@@ -57,6 +110,7 @@ function update(user) {
             ...data.items[index],
             ...user
         };
+        save().catch(console.error);
         return user;
     }
     return null;
@@ -70,6 +124,7 @@ function remove(id) {
     const index = data.items.findIndex(item => item.id == id);
     if (index >= 0) {
         const deleted = data.items.splice(index, 1);
+        save().catch(console.error);
         return deleted[0];
     }
     return null;
