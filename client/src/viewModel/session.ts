c7f4c9376e1707d5c945638adae6c98d6af836a0
lemusm3 <lemusm3@newplatz.edu>
2024-04-18 14:27:55 -0400

    created session based api function

--- client/src/viewModel/session.ts
+++ client/src/viewModel/session.ts
@@ -1,9 +1,11 @@
 /* B"H
 */
 
+import type { DataEnvelope } from "@/model/transportTypes";
 import type { User } from "@/model/users";
 import { reactive } from "vue";
 import { useRouter } from "vue-router";
+import * as myFetch from "@/model/myFetch";
 
 const session  = reactive({
     user: null as User | null,
@@ -25,3 +27,6 @@ export function useLogin() {
 
 export const refSession = () => session;
 
+export function api<T>(action: string): Promise< DataEnvelope<T> >{
+    return myFetch.api<T>(action);
+}
