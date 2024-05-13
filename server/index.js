cef4eec74a20e079ae4e48b6084bafdfb163e6d3
lemusm3 <lemusm3@newplatz.edu>
2024-05-13 13:22:03 -0400

    Update server configuration and add API endpoint for products

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
+require('dotenv').config();
+const path = require('path');
 const express = require('express');
 const users = require('./controllers/users');
+const products = require('./controllers/products');
 /* B"H
 
 Four types of Asynchronous code:
@@ -10,19 +13,55 @@ Four types of Asynchronous code:
 
 */
 
+/**  
+ * @typedef {import('../client/src/model/transportTypes').DataEnvelope<null> } ErrorDataEnvelope
+ * */
+
+
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT ?? 3000;
+
+console.log('The best class at New Paltz is:', process.env.BEST_CLASS);
 
 app
+// Static files
+  .use(express.static('client/dist'))
+
+// Middleware
   .use(express.json())
+  .use((req, res, next) => {
+    
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+
+    next();
+  })
+
 
 app
   .get('/', (req, res) => {
     res.send('Hello New Paltz!')
   })
   .use('/api/v1/users', users)
+  .use('/api/v1/products', products)
+
+// 404
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname,  '../client/dist/index.html'));
+})
 
 // Error handling
+app.use((err, req, res, next) => {
+  console.error(err);
+  /** @type {ErrorDataEnvelope } */
+  const results = { 
+    isSuccess: false,
+    message: err.message || 'Internal Server Error',
+    data: null,
+   };
+  res.status(500).send(results);
+})
 
 
 app.listen(PORT, () => {
