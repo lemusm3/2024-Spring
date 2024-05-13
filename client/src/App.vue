9866a0a63dc828456cb0ad5900ff16f87e1d2461
lemusm3 <lemusm3@newplatz.edu>
2024-05-13 13:17:29 -0400

    Add loading progress bar to App.vue and update main.ts

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import NavBar from './components/NavBar.vue';
+import { refSession } from './viewModel/session';
 
+const session = refSession();
 </script>
 
 <template>
   <div class="wrapper">
     <NavBar />
     <div class="container">
+      <progress class="progress is-small is-primary" max="100" v-show="session.isLoading"></progress>
       <RouterView /> 
     </div>
   </div>
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,11 +1,15 @@
 import './assets/main.css'
+import 'vue-toastification/dist/index.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import Toaster from 'vue-toastification'
 
 const app = createApp(App)
 
-app.use(router)
+app
+    .use(router)
+    .use(Toaster, {})
 
 app.mount('#app')
