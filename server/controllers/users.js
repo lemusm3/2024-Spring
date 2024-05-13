850610633bcf728bfa503e3adca077187487ff43
lemusm3 <lemusm3@newplatz.edu>
2024-05-13 13:22:17 -0400

    user controller to use promises for async operations

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -20,76 +20,96 @@ const app = express.Router();
 
 app
     .get('/', (req, res, next) => {
-        const all = users.getAll();
-        /** @type { UserDataListEnvelope } */
-        const response = {
-            data: all,
-            totalCount: all.length,
-            isSuccess: true,
-        }
-        res.send(response);
+        users.getAll()
+        .then(all => {
+            /** @type { UserDataListEnvelope } */
+            const response = {
+                data: all,
+                totalCount: all.length,
+                isSuccess: true,
+            }
+            res.send(response);
+        }).catch(next);
         
     })
     .get('/search', (req, res, next) => {
 
         const search = req.query.q;
         if(typeof search !== 'string' ) throw new Error('search is required');
-        const result = users.search(search);
-        /** @type { UserDataListEnvelope } */
-        const response = {
-            data: result,
-            totalCount: result.length,
-            isSuccess: true,
-        }
-        res.send(response);
-
+        users.search(search)
+        .then(result => {
+            /** @type { UserDataListEnvelope } */
+            const response = {
+                data: result,
+                totalCount: result.length,
+                isSuccess: true,
+            }
+            res.send(response);
+        }).catch(next);
     })
     .get('/:id', (req, res, next) => {
         const id = req.params.id;
-        /** @type { UserDataEnvelope } */
-        const response = {
-            data: users.get(+id),
-            isSuccess: true,
-        }
-        res.send(response);
+        users.get(+id)
+        .then(result => {
+            /** @type { UserDataEnvelope } */
+            const response = {
+                data: result,
+                isSuccess: true,
+            }
+            res.send(response);
+        }).catch(next);
     })
     .post('/', (req, res, next) => {
         const user = req.body;
-        const result = users.add(user);
-
-        /** @type { UserDataEnvelope } */
-        const response = {
-            data: result,
-            isSuccess: true,
-        }
-        
-        res.send(response);
+        console.log("1: About to add user");
+        users.add(user)
+        .then(result => {
+            console.log("5: Returned from add user");
+            /** @type { UserDataEnvelope } */
+            const response = {
+                data: result,
+                isSuccess: true,
+            }
+            res.send(response);
+        }).catch(next);
+    })
+    .post('/login', (req, res, next) => {
+        const { email, password } = req.body;
+        users.login(email, password)
+        .then(result => {
+            /** @type { UserDataEnvelope } */
+            const response = {
+                data: result,
+                isSuccess: true,
+            }
+            res.send(response);
+        }).catch(next);
     })
     .patch('/:id', (req, res, next) => {
         const user = req.body;
         user.id = req.params.id;
-        const result = users.update(user);
+        users.update(user)
+        .then(result => {
+            /** @type { UserDataEnvelope } */
+            const response = {
+                data: result,
+                isSuccess: true,
+            }
 
-        /** @type { UserDataEnvelope } */
-        const response = {
-            data: result,
-            isSuccess: true,
-        }
-        
-        res.send(response);
+            res.send(response);
+        }).catch(next);
     })
     .delete('/:id', (req, res, next) => {
         const id = req.params.id;
-        const result = users.remove(+id);
-
-        
-        /** @type { UserDataEnvelope } */
-        const response = {
-            data: result,
-            isSuccess: true,
-        }
-        
-        res.send(response);
+        users.remove(+id)
+        .then(result => {
+            /** @type { UserDataEnvelope } */
+            const response = {
+                data: result,
+                isSuccess: true,
+            }
+            res.send(response);
+        }).catch(next);
     })
 
 
