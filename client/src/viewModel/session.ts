3b7a165d3f1912c23250df2739039d78e04d73d5
lemusm3 <lemusm3@newplatz.edu>
2024-05-13 13:20:31 -0400

    Update API calls to use data envelopes

--- client/src/viewModel/session.ts
+++ client/src/viewModel/session.ts
@@ -1,20 +1,27 @@
 /* B"H
 */
 
+import type { DataEnvelope } from "@/model/transportTypes";
 import type { User } from "@/model/users";
 import { reactive } from "vue";
 import { useRouter } from "vue-router";
+import * as myFetch from "@/model/myFetch";
+import { useToast } from "vue-toastification";
 
 const session  = reactive({
     user: null as User | null,
+    isLoading: 0,
 });
 
 export function useLogin() {
     const router = useRouter();
     return {
-        login(user: User) {
-            session.user = user;
-            router.push("/products");
+        async login(user: User) {
+            const x = await api<User>("users/login", user);
+            if(x){
+                session.user = x.data;
+                router.push("/");
+            }
         },
         logout() {
             session.user = null;
@@ -25,3 +32,21 @@ export function useLogin() {
 
 export const refSession = () => session;
 
+export function api<T>(action: string, data?: unknown, method?: string){
+    session.isLoading++;
+    return myFetch.api<T>(action, data, method)
+    .then(x=>{
+        if(!x.isSuccess){
+            showError(x);
+        }
+        return x;
+    })
+    .catch(showError)
+    .finally(() => session.isLoading--);
+}
+
+const toast = useToast();
+export function showError(error: any){
+    console.error(error);
+    toast.error(error.message || error);
+}
